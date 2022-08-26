7353bddfbb6290ed0d00bcd6361bcb9bff633717
Auggie <aneff6@huskers.unl.edu>
2022-08-26 03:41:20 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,6 +1,18 @@
+/**
+ * Author: Auggie
+ *         EMAIL
+ * Date: 2022/08/23
+ *
+ * A simple introduction
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
 int main(int argc, char **argv) {
 
-  printf("Auggie!\n");
-  printf("Electrical engineering\n");
-return 0;
+  printf("Auggie Neff\n");
+  printf("My major is Electrical Engineering\n");
+
+  return 0;
 }
