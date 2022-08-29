3422aeb2810958a4dc372e8e9cfa89b1d1f536f5
AuggieNeff <111907325+AuggieNeff@users.noreply.github.com>
2022-08-29 00:26:37 -0500

    Add files via upload

---
+++ hello/hello (1).c
@@ -0,0 +1,18 @@
+/**
+ * Author: Auggie
+ *         EMAIL
+ * Date: 2022/08/23
+ *
+ * A simple introduction
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Auggie Neff\n");
+  printf("My major is Electrical Engineering\n");
+
+  return 0;
+}
